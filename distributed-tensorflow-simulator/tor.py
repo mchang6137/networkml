9ccd4293b4c92f563f06c07fb572da8fb9430863
Domenic Bottini <dbottini@berkeley.edu>
2018-01-17 00:27:32 -0800

    fixed merge issues

--- distributed-tensorflow-simulator/tor.py
+++ distributed-tensorflow-simulator/tor.py
@@ -3,4 +3,11 @@ from entity import Entity
 class TOR (Entity):
     def __init__(self, ctx, name="TOR", inbuffer_size=0):
         Entity.__init__(self, ctx, name=name, inbuffer_size=inbuffer_size)
+        self.fromworkers = {}
 
+    def lastbitrecv(self, packet):
+        if packet.src not in self.ctx.workers or not self.ctx.in_network_computation:
+            Entity.lastbitrecv(self, packet)
+        else:
+            if not packet.MF:
+                self.fromworkers[packet.name]
