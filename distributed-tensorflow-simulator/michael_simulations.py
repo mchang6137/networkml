abe1576e386dca9fa9bb58c1c0ac073d6e7d08a7
michael chang <michaelc@berkeley.edu>
2018-01-23 01:35:42 -0800

    Michael simulations to accomodate for the worker receive times

--- distributed-tensorflow-simulator/michael_simulations.py
+++ distributed-tensorflow-simulator/michael_simulations.py
@@ -9,6 +9,12 @@ from simulator import *
 Michael's Means of automating experiments
 '''
 
+def run_sim(args):
+    sim = Simulation()
+    sim.Setup(args)
+    finish_time, worker_receive_times = sim.Run()
+    write_to_csv(args, finish_time, worker_receive_times)
+    
 def vary_bandwidths(args):
     bw_candidates = [10,1]
 
@@ -23,15 +29,21 @@ def vary_bandwidths(args):
 
 # Try various kinds of models and 
 def vary_model(args):
-    model_candidates = ['inception-v3', 'resnet-200']
+    model_candidates = ['inception-v3', 'resnet-200', 'resnet-101', 'vgg16']
 
     fw_pass_time = {'inception-v3': 0.176,
                     'resnet-200': 0.357,
                     'vgg16': 0.169,
                     'resnet-101': 0.176}
 
+    step_num = {'inception-v3': 40,
+                'resnet-200': 40,
+                'resnet-101': 40,
+                'vgg16': 28}
+
     for model_name in model_candidates:
         args.model_name = model_name
+        args.step_num = step_num[model_name]
         args.fw_pass_time = fw_pass_time[model_name]
         args.trace_base_dir = 'csv/' + model_name + '/'
         args.json = 'json/' + '{}_param_ps_assignment.json'.format(model_name)
@@ -52,10 +64,7 @@ def vary_workers_exp_multicast(args, num_workers=[2,4,8,12], num_ps=[1,2,4,8]):
             args.use_multicast = 1
             args.in_network_computation = 0
             try:
-                sim = Simulation()
-                sim.Setup(args)
-                finish_time = sim.Run()
-                write_to_csv(args, finish_time)
+                run_sim(args)
             except:
                 print 'this experiment failed in multicast fct'
 
@@ -72,10 +81,7 @@ def vary_workers_exp_aggregation(args, num_workers=[2,4,8,12], num_ps=[1,2,4,8])
             args.use_multicast = 0
             args.in_network_computation = 1
             try:
-                sim = Simulation()
-                sim.Setup(args)
-                finish_time = sim.Run()
-                write_to_csv(args, finish_time)
+                run_sim(args)
             except:
                 print 'this experiment failed in aggregation part'
 
@@ -91,10 +97,7 @@ def vary_workers_exp(args, num_workers=[2,4,8,12], num_ps=[1,2,4,8]):
             args.use_multicast = 0
             args.in_network_computation = 0
             try:
-                sim = Simulation()
-                sim.Setup(args)
-                finish_time = sim.Run()
-                write_to_csv(args, finish_time)
+                run_sim(args)
             except:
                 print 'this experiment failed in no network improvement'
 
@@ -109,10 +112,7 @@ def vary_workers_exp_multicast_aggregation(args, num_workers=[2,4,8,12], num_ps=
             args.use_multicast = 1
             args.in_network_computation = 1
             try:
-                sim = Simulation()
-                sim.Setup(args)
-	        finish_time = sim.Run()
-                write_to_csv(args, finish_time)
+                run_sim(args)
             except:
                 print 'This experiment has failed in both the agg and multicast'
 
